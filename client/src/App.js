7d347e9c4f62ece5331891b88c2160436a8a3753
abedalisei <a14-3@hotmail.co.uk>
2023-04-18 16:04:12 +0100

    Implemented App.js routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
-import { useEffect } from 'react'
-import axios from 'axios'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './components/pages/HomePage'
+import ChallengesPage from './components/pages/ChallengesPage'
+import ChallengeIndivPage from './components/pages/ChallengeIndivPage'
+import LeaderboardPage from './components/pages/LeaderboardPage'
+import PageNotFound from './components/pages/PageNotFound'
+import NavBar from './components/header/NavBar'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
 
 const App = () => {
-  useEffect(() => {
-    const getData = async () => {
-      const { data } = await axios.get('/api/leaderboard/') // * <-- replace with your endpoint
-      console.log(data)
-    }
-    getData()
-  })
-
-  return <h1>Hello World</h1>
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/challenges' element={<ChallengesPage />} />
+        <Route path='/challenges/:challengeId' element={<ChallengeIndivPage />} />
+        <Route path='/leaderboard' element={<LeaderboardPage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
