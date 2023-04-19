6172012c50eb3d2038309829ca4f31901f4a99a4
abedalisei <a14-3@hotmail.co.uk>
2023-04-19 04:57:08 +0100

    Amended default MUI breakpoint

--- client/src/App.js
+++ client/src/App.js
@@ -8,21 +8,25 @@ import PageNotFound from './components/pages/PageNotFound'
 import NavBar from './components/header/NavBar'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
+import { ThemeProvider } from '@mui/material/styles'
+import customTheme from './theme/theme'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/challenges' element={<ChallengesPage />} />
-        <Route path='/challenges/:challengeId' element={<ChallengeIndivPage />} />
-        <Route path='/leaderboard' element={<LeaderboardPage />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='*' element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={customTheme}>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/challenges' element={<ChallengesPage />} />
+          <Route path='/challenges/:challengeId' element={<ChallengeIndivPage />} />
+          <Route path='/leaderboard' element={<LeaderboardPage />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
---
+++ client/src/theme/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+const customTheme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 768,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
+
+export default customTheme
